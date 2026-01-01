ace7b21e759013bea4501246c3be2e4df318d67a
Ameer <Ameer.Saleem@warwick.ac.uk>
2026-01-01 16:02:24 +0000

    Initial commit

---
+++ src/clean_data.py
@@ -0,0 +1,52 @@
+import os
+import pandas as pd
+
+def clean_data(file_path: str, file_name: str = 'data_raw.csv') -> pd.DataFrame:
+    """
+    Reads in data from the raw Strong App .csv file and cleans it.
+
+    Parameters
+    ----------
+    file_path : str
+        The path to the raw Strong App data file.
+    file_name : str
+        The name of the raw Strong App data file (including extension).
+
+    Returns
+    ----------
+    cleaned_df: pd.DataFrame
+        The cleaned DataFrame.
+    """
+    # Read in data
+    first_col_name = 'Workout #;"Date";"Workout Name";"Duration (sec)";"Exercise Name";"Set Order";"Weight (kg)";"Reps";"RPE";"Distance (meters)";"Seconds";"Notes";"Workout Notes"'
+
+    df = pd.read_csv(
+        os.path.join(file_path, file_name),
+        header=1,
+        names=[first_col_name, 'Extra'],
+        usecols=[0, 1]
+    )
+
+    second_col_name = df.columns[1]
+    df[second_col_name] = df[second_col_name].fillna('')
+
+    # Combine all relevant data into the first column
+    df[first_col_name] = df[first_col_name] + df[second_col_name]
+
+    new_cols = first_col_name.replace('"', '').split(';')
+
+    # Clean text data
+    list_series = df[first_col_name].apply(lambda x: x.replace('"', '').split(';'))
+    cleaned_df = list_series.apply(pd.Series)
+
+    # Drop any extra columns
+    cleaned_df = cleaned_df.iloc[:, :len(new_cols)]
+    cleaned_df.columns = new_cols
+
+    # Convert data types
+    num_cols = ['Workout #', 'Duration (sec)', 'Weight (kg)', 'Reps', 'RPE', 'Distance (meters)', 'Seconds']
+    cleaned_df[num_cols] = cleaned_df[num_cols].apply(pd.to_numeric, errors='coerce')
+
+    # Could potentially drop null values here too
+
+    return cleaned_df
